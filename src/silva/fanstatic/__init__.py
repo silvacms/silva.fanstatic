d1c84bb14c531c6ef5c300ba921fbf5228305835
Sylvain Viollon <thefunny@gmail.com>
2011-08-30 16:18:10 +0200

    Fix dummy needed resources for testing.

--- src/silva/fanstatic/__init__.py
+++ src/silva/fanstatic/__init__.py
@@ -6,3 +6,10 @@ from silva.fanstatic.extending import need
 
 __all__ = ['need']
 
+
+# Patch fanstatic with missing API calls during testing
+from fanstatic.core import DummyNeededResources
+
+DummyNeededResources.has_base_url = lambda self: True
+DummyNeededResources.set_base_url = lambda self, url: None
+DummyNeededResources.library_url = lambda self, library: u'/'.join(('http://test', library.name))
